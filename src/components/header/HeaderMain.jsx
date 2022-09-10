b90282ba800b88f9ce5b712890c10a3b1515318e
anthony <arodriguez@611digital.com>
2022-09-10 00:12:15 -0400

    Created Header, maked responsive for a variety of resolutions, and created usefull variables css.

---
+++ src/components/header/HeaderMain.jsx
@@ -0,0 +1,18 @@
+import '../../App.scss'
+
+function HeaderMain() {
+
+    return (
+        <div className="HeaderMain header">
+            <div className="center-bg"></div>
+            <div className="bottom-bg"></div>
+            <div className="z-index-100">
+                <h1 className="title-main">
+                    Pokedéx
+                </h1>
+            </div>
+        </div>
+    )
+}
+
+export default HeaderMain
---
+++ src/views/App.jsx
@@ -0,0 +1,13 @@
+import '../App.scss'
+import HeaderMain from "../components/header/HeaderMain.jsx";
+
+function App() {
+
+  return (
+    <div className="App">
+        <HeaderMain/>
+    </div>
+  )
+}
+
+export default App
