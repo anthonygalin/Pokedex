e5e03ccfd6ce0c2f65c3bdf15b524c035657223e
anthony <arodriguez@611digital.com>
2022-09-11 15:42:39 -0400

    SE CAMBIO EL COMPONENTE NAME POR UN DIV EN VEZ DE UN BUTTON.

--- src/components/main/pokemon/Name.jsx
+++ src/components/main/pokemon/Name.jsx
@@ -4,7 +4,7 @@ function Name({ pokeData }) {
 
     return (
         <section className="info-btn-container align-self-end">
-            <button className="info-btn"><h1 className="pokemon-name">{ pokeData.name }</h1></button>
+            <div className="info-btn text-align-center"><h1 className="pokemon-name">{ pokeData.name }</h1></div>
         </section>
     )
 }
